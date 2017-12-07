c20ccc2b982cf39f950e442718c2fc4dc0acccab
Krishnaamen <krishnaamen@gmail.com>
2017-12-07 05:07:29 +0100

    Initial Commit

---
+++ Danish Vocabulary Trainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Danish Vocabulary Trainer
+//
+//  Created by Krishna Prasad Khanal on 07/12/2017.
+//  Copyright © 2017 Krishna Prasad Khanal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
